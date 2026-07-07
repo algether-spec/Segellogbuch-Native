928add29cbba070cb789471a8668a5bf56609012
Alois Gether <al.gether@gmail.com>
2026-07-07 14:07:42 +0200

    [config] Versionssuffix -dev für dev-Branch eingeführt (v2.5.175-dev)

--- www/config.js
+++ www/config.js
@@ -3,7 +3,7 @@
    App-Version und Auto-Update-Logik
 ====================== */
 
-const APP_VERSION = "2.5.175";
+const APP_VERSION = "2.5.175-dev";
 let _updateIntervalId = null;
 
 function updateButtonInit() {
