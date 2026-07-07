2f1ea3211e37f1d4107581765a304300bccfc1f4
Alois Gether <al.gether@gmail.com>
2026-07-07 17:50:30 +0200

    [pdf] Unterschriften-Seite: Von/Bis-Tabelle aller Schiffsführer-Abschnitte

--- www/config.js
+++ www/config.js
@@ -3,7 +3,7 @@
    App-Version und Auto-Update-Logik
 ====================== */
 
-const APP_VERSION = "2.5.177-dev";
+const APP_VERSION = "2.5.178-dev";
 let _updateIntervalId = null;
 
 function updateButtonInit() {
